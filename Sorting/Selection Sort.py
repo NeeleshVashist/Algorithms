12f39e40a247bb3d3a07fbf188aa83a929b03e61
Neelesh Vashist <neelesh.vashist@gmail.com>
2020-12-31 00:51:43 +0530

    SelectionSort

---
+++ Sorting/Selection Sort.py
@@ -0,0 +1,23 @@
+# Selection Sort Logic
+# 
+# Time Complexcity:     O(n2)
+# Space Complexcity:    O(1)
+
+def SelectionSort(nums, array_len):
+    for i in range(array_len):
+        max_num = i
+        for j in range(i+1, array_len):
+            if(nums[max_num] > nums[j]):
+                max_num = j
+        
+        nums[i], nums[max_num] = nums[max_num], nums[i]
+
+    print(nums)
+
+nums = []
+array_len = int(input("Enter Length of Array: "))
+for i in range(array_len):
+    num = int(input("Enter number: "))
+    nums.append(num)
+
+SelectionSort(nums, array_len)
